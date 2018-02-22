cff04de63475e57bf5254d54aa140d172aca5db2
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-22 17:01:45 -0600

    JS now outputs bundle on build. Detox still has an issue with the way the bundle is being loaded. App can at least attempt to load the bundle

--- ios/react-native-app/AppDelegate.m
+++ ios/react-native-app/AppDelegate.m
@@ -19,11 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSURL *jsCodeLocation;
     
-#ifdef DEBUG
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-#else
     jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
     
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                         moduleName:@"example"
