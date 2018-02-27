1581dae79a60b15a210522d4b57bf7f8ebf9d725
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-27 15:54:47 -0600

    Typescript working on device

--- ios/fizzreactnative/AppDelegate.m
+++ ios/fizzreactnative/AppDelegate.m
@@ -18,7 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"lib/index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"fizzreactnative"
