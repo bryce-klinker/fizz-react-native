8deea0126f498a5c866a6246de46f1faf8a1f118
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-27 15:17:19 -0600

    Reset to zero to see if ejecting from create-react-native-app helps with issues in scripts

--- index.ios.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import React, { Component } from 'react';
-import App from "./App";
-
-AppRegistry.registerComponent('react-native-app', () => App);
--- ios/.bundle/ruby/2.3.0/gems/molinillo-0.6.4/lib/molinillo/gem_metadata.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Molinillo
-  # The version of Molinillo.
-  VERSION = '0.6.4'.freeze
-end
