b9568f489df0b8c4d6a460131eb596b6fd724c5a
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-08 15:39:25 -0600

    Hacking some more on the golden hour guage.

--- src/dashboard/components/GoldenHourGuage.tsx
+++ src/dashboard/components/GoldenHourGuage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Dimensions, Image, Text, View, ViewStyle} from 'react-native';
 import {ProgressCircle} from 'react-native-svg-charts';
 import {COLORS} from '../../shared/styles/colors';
 import {DashboardModel} from '../models/dashboard.model';
@@ -11,20 +11,27 @@ export interface GoldenHourGaugeProps {
 export const GoldenHourGauge = (props: GoldenHourGaugeProps) => {
     const { model } = props;
     const progress = ((Date.now() - Date.parse(model.checkInTimestamp)) / 60000) / 60;
+    const style: ViewStyle = {
+        height: Dimensions.get('window').height - 100,
+        width: Dimensions.get('window').width - 100,
+    };
+
     return (
-        <View>
+        <View >
             <Text>Golden Hour</Text>
-            <View>
-                <Image source={require('../../shared/icons/checkin.png')}/>
-                <Text>{model.checkInTimestamp}</Text>
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <ProgressCircle
+                    style={style}
+                    progress={progress}
+                    progressColor={COLORS.primary}
+                    startAngle={-Math.PI * 0.8}
+                    endAngle={Math.PI * 0.8}
+                />
+                <View style={{left: 65, top: 365, position: 'absolute'}}>
+                    <Image source={require('../../shared/icons/checkin.png')} style={{borderRadius: 10, borderColor: COLORS.primary, borderWidth: 2}}/>
+                    <Text>{model.checkInTimestamp}</Text>
+                </View>
             </View>
-            <ProgressCircle
-                style={{height: 200}}
-                progress={progress}
-                progressColor={COLORS.primary}
-                startAngle={-Math.PI * 0.8}
-                endAngle={Math.PI * 0.8}
-            />
         </View>
     );
 };
